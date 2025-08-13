14c39994bfd391f4ab78715882cdcfa0684e6907
KnickQueue <155283151+KnickQueue@users.noreply.github.com>
2025-08-13 13:35:25 -0400

    Disable Room schema export

--- app/src/main/java/com/example/kronosclock/data/WatchDatabase.kt
+++ app/src/main/java/com/example/kronosclock/data/WatchDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Watch::class], version = 1)
+@Database(entities = [Watch::class], version = 1, exportSchema = false)
 abstract class WatchDatabase : RoomDatabase() {
     abstract fun watchDao(): WatchDao
 
