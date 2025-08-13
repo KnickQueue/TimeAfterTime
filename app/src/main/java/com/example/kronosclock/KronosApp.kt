994ec50e8002825020ba9536df45cdf910222e81
KnickQueue <155283151+KnickQueue@users.noreply.github.com>
2025-08-13 12:37:58 -0400

    Add Compose foundation dependency and organize theme

--- app/src/main/java/com/example/kronosclock/KronosApp.kt
+++ app/src/main/java/com/example/kronosclock/KronosApp.kt
@@ -1,12 +1,14 @@
-package com.example.kronosanalogclock
+package com.example.kronosclock
 
 import android.app.Application
 import com.lyft.kronos.AndroidClockFactory
 import com.lyft.kronos.KronosClock
+import com.example.kronosclock.data.WatchDatabase
 
 class KronosApp : Application() {
     lateinit var kronos: KronosClock
         private set
+    val database: WatchDatabase by lazy { WatchDatabase.getInstance(this) }
 
     override fun onCreate() {
         super.onCreate()
